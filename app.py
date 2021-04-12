c47c5a6c8df0482052d40c68b2c3057d48cf960f
anthonysokolov <anthonysokolov20@gmail.com>
2021-04-11 21:41:56 -0400

    add file uploading

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from werkzeug import secure_filename
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'user_data'
 
 @app.route('/')
 def landing_page():
-    return 'pls upload a photo/video file'
+    return render_template('upload.html')
+	
+@app.route('/uploader', methods = ['GET', 'POST'])
+def upload_file():
+   if request.method == 'POST':
+      f = request.files['file']
+      f.save(secure_filename(f.filename))
+      return 'file uploaded successfully'
 
 app.run()
